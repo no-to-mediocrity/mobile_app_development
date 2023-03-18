ce738166717239c7f9fefb03f726f72d65653acf
Alex <103932823+no-to-mediocrity@users.noreply.github.com>
2023-03-19 00:58:48 +0200

    Update task2.kt

--- homework/task2.kt
+++ homework/task2.kt
@@ -5,22 +5,27 @@ fun main() {
     println("Enter a time value in the format of 'HH:mm':")
     val input = scanner.nextLine()
     val (hours, minutes) = parseInput(input)
-    val mirrorTime = calculateMirrorTime(hours, minutes)
+    var mirrorTime = calculateMirrorTime(hours, minutes)
     println("Mirror time: ${formatTime(mirrorTime)}")
 }
 
 fun parseInput(input: String): Pair<Int, Int> {
     require(input.length == 5 && input[2] == ':') { "Invalid input: $input" }
     val hours = input.substring(0, 2).toIntOrNull()
-    require(hours != null && hours in 0..11) { "Invalid hours: $hours" }
+    require(hours != null && hours in 0..12) { "Invalid hours: $hours" }
     val minutes = input.substring(3, 5).toIntOrNull()
     require(minutes != null && minutes in 0..59) { "Invalid minutes: $minutes" }
     return Pair(hours, minutes)
 }
 
 fun calculateMirrorTime(hours: Int, minutes: Int): Pair<Int, Int> {
-    val mirrorHours = 12 - hours
+    var mirrorHours: Int?
     val mirrorMinutes = 60 - minutes
+    if ((hours == 0) || (hours == 12)) {
+        mirrorHours = hours
+    } else {
+        mirrorHours =  12 - hours
+    }
     return Pair(mirrorHours, mirrorMinutes)
 }
 
